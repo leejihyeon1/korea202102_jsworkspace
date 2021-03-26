7a9193de62d7bd1a8f9486ad427b85466097aecd
leejihyeon1 <55727503+leejihyeon1@users.noreply.github.com>
2021-03-26 23:46:06 +0900

    Add files via upload

---
+++ js/store.js
@@ -0,0 +1,52 @@
+// 역삼동 맛집 정보 json
+var store={
+    "local":"역삼동",
+    "theme":"맛집",
+    "storeList":[
+       {
+           "name":"밀빵떡볶이",
+           "latLng":{
+                "lat":37.50091705137511, 
+                "lng":127.03310937358229
+           },
+           "image":"../images/store/ricecake.jpg",
+           "review":"20년간 장사를 해온 이집은 최고의 맛집"
+       },        
+       {
+           "name":"더후라이팬",
+           "latLng":{
+                "lat":37.501294236579184, 
+                "lng":127.0330118102085
+           },
+           "image":"../images/store/fripan.jpg",
+           "review":"존맛탱입니다 강추!"
+       },        
+       {
+           "name":"삼곱식당",
+           "latLng":{
+                "lat":37.50269170628681, 
+                "lng":127.03721643328917
+           },
+           "image":"../images/store/samgob.jpg",
+           "review":"일주일에 두번씩은 꼭 먹는 맛집 "
+       },        
+       {
+           "name":"이태리부대찌게",
+           "latLng":{
+                "lat":37.502935868017275, 
+                "lng":127.03815809523849
+           },
+           "image":"../images/store/italy.jpg",
+           "review":"이것이 진정한 밥도둑"
+       },        
+       {
+           "name":"우리집",
+           "latLng":{
+                "lat":37.503329079071506, 
+                "lng":127.03884020356904
+           },
+           "image":"../images/store/woori.jpg",
+           "review":"3대가 이어온 맛집"
+       }
+    ]
+}
